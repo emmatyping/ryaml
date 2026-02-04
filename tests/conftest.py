6d8b0421b9da847e41c4a16116f93118871695ac
Emma Harper Smith <emma@emmatyping.dev>
2026-02-04 04:10:44 +0000

    Open test file in utf8 mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import io
 
 @pytest.fixture
 def yaml_file(tmp_path):
-    with open(tmp_path / 'testfile.yaml', 'w+') as y:
+    with open(tmp_path / 'testfile.yaml', 'w+', encoding='utf8') as y:
         assert isinstance(y, io.TextIOBase)
         yield y
